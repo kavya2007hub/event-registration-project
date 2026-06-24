f2434081264aa365a799c5031fbcd0e01f317dae
kavya2007hub <sumathi010781@gmail.com>
2026-06-24 14:25:05 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,8 @@
+function register(){
+    let eventName = "Mira Maestroes 2026"
+    console.log(eventName);
+    console.log("Registration Foem Submitter");
+    let year = 2026;
+    console.log(year);
+    window.location = "project thankyou.html";
+}
